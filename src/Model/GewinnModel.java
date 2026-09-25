ec48baef9057e144bafeaf4f95b6395e3670c3cd
Dario Duric <djuroooopbuy@gmail.com>
2026-09-25 18:19:01 +0200

    verbesserung von GewinnModel

--- src/Model/GewinnModel.java
+++ src/Model/GewinnModel.java
@@ -1,23 +1,18 @@
 package Model;
-public class GewinnModel {
-  private int gesamtPunkte = 30;
-  private int spielerZahl;
-  private int computerZahl;
-  private int rundenErgebnis;
 
-    public GewinnModel(int spielerZahl, int gesamtPunkte, int computerZahl, int rundenErgebnis) {
-        this.spielerZahl = spielerZahl;
-        this.gesamtPunkte = gesamtPunkte;
-        this.computerZahl = computerZahl;
-        this.rundenErgebnis = rundenErgebnis;
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    // Parameterloser Konstruktor: Startet mit 30 Punkten
+    public GewinnModel() {
+        this.gesamtPunkte = 30;
     }
 
     public int getGesamtPunkte() {
         return gesamtPunkte;
-    }
-
-    public int getSpielerZahl() {
-        return spielerZahl;
     }
 
     public int getComputerZahl() {
@@ -27,25 +22,36 @@ public class GewinnModel {
     public int getRundenErgebnis() {
         return rundenErgebnis;
     }
+
+    public int getSpielerZahl() {
+        return spielerZahl;
+    }
+
+    // Zufallszahl von 1 bis 9 für den Computer
     public void berechneComputerZahl() {
         this.computerZahl = (int) (Math.random() * 9) + 1;
     }
-    public boolean hatGewonnen() {
-        return this.gesamtPunkte >= 100;
-    }
-    public boolean hatVerloren() {
-        return this.gesamtPunkte <= 0;
-    }
+
+    // Rundenberechnung ohne Math.abs
     public void berechneRunde(int spielerZahl) {
         this.spielerZahl = spielerZahl;
-        if(this.spielerZahl == this.computerZahl) {
+
+        if (this.spielerZahl == this.computerZahl) {
             this.rundenErgebnis = 20;
-        } else if (this.spielerZahl == this.computerZahl +1 || this.spielerZahl == this.computerZahl -1) {
+        } else if (this.spielerZahl == this.computerZahl + 1 || this.spielerZahl == this.computerZahl - 1) {
             this.rundenErgebnis = 5;
         } else {
             this.rundenErgebnis = -10;
         }
+
         this.gesamtPunkte += this.rundenErgebnis;
     }
-}
 
+    public boolean hatGewonnen() {
+        return this.gesamtPunkte >= 100;
+    }
+
+    public boolean hatVerloren() {
+        return this.gesamtPunkte <= 0;
+    }
+}
